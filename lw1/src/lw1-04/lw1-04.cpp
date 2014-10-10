e1993c43b95598963af5446aa448e2dc70105471
IvanUskov <ivan.uskov@ispringsolutions.com>
2014-10-10 04:53:31 +0400

    Сделал lw1-04

---
+++ lw1/src/lw1-04/lw1-04.cpp
@@ -0,0 +1,80 @@
+#include "stdafx.h"
+#include <cfloat>
+
+const double MIN_N = 2;
+const double FIRST_FIB_NUM = 0;
+const double SECOND_FIB_NUM = 1;
+
+const int ERROR_TO_BIG_INPUT_VALUE = 0;
+const int SUCCESS = 1;
+
+const int ELEMENTS_IN_ROW = 5;
+
+bool checkCanPrintSequence(const double n)
+{
+    return (n > MIN_N && ((double)((int)n) == n));
+}
+
+bool checkCanAddup(double x, double y)
+{
+    return ( (DBL_MAX - x) > y );
+}
+
+int printSequence(const double n)
+{
+    double untilLast = FIRST_FIB_NUM;
+    double last = SECOND_FIB_NUM;
+    double curr;
+
+    int newLineCounter = ELEMENTS_IN_ROW;
+
+    for (double i = MIN_N; i <= n; i++)
+    {
+        if (!checkCanAddup(untilLast, last))
+        {
+            return ERROR_TO_BIG_INPUT_VALUE;
+        }
+
+        curr = untilLast + last;
+        untilLast = last;
+        last = curr;
+
+        printf("%15.0f ", curr);
+        newLineCounter--;
+
+        if (newLineCounter == 0)
+        {
+            newLineCounter = ELEMENTS_IN_ROW;
+            printf("\n");
+        }
+    }
+    return SUCCESS;
+}
+
+int main(int argc, char* argv[])
+{ 
+    if (argc <= 1)
+    {
+        printf("\nPlease enter max number - N >= 2\n");
+        return 1;
+    }
+
+    const double n = strtod(argv[1], NULL);
+
+    bool canPrintSequence = checkCanPrintSequence(n);
+    if (canPrintSequence)
+    {
+        int code = printSequence(n);
+        if (code == ERROR_TO_BIG_INPUT_VALUE)
+        {
+            printf("\nSorry, too big input max number\n");
+        }
+    }
+    else
+    {
+        printf("\nInvalid argument, its sholud be natural number N >= 2\n");
+        return 1;
+    }
+    printf("\n");
+    return 0;
+}
